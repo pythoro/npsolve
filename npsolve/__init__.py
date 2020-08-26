53d4c9dc54f18ee7a118280cab6c9ebde307f3a4
pythoro <pythoro@mindquip.com>
2020-08-27 11:19:09 +1200

    [ADD] New basic module for simplified array index management.

--- npsolve/__init__.py
+++ npsolve/__init__.py
@@ -6,6 +6,7 @@ Created on Mon Aug  5 20:46:26 2019
 """
 
 from . import settings
+from . import basic
 from .core import Partial, Solver
 from .cache import multi_cached, mono_cached
 from . import soft_functions
---
+++ npsolve/basic.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 27 10:41:13 2020
+
+@author: Reuben
+
+A lightweight module for simplified array index management.
+
+"""
+
+import numpy as np
+
+class V_Set():
+    def __init__(self, names=None):
+        self._dct = {}
+        if names is not None:
+            self.add(names)
+    
+    def add(self, names):
+        if isinstance(names, str):
+            names = names.split(' ')
+        for name in names:
+            if name not in self._dct:
+                self._dct[name] = len(self._dct)
+    
+    def __call__(self, names=None):
+        if names is None:
+            lst = list(self._dct.keys()) if names is None else names
+        elif isinstance(names, str):
+            lst = names.split(' ')
+        elif isinstance(names, list):
+            lst = names
+        else:
+            raise KeyError("Argument 'names' 'not understood.")
+        if len(lst) == 1:
+            return self._dct[lst[0]]
+        else:
+            return [self._dct[n] for n in lst]
+    
+    def __getitem__(self, name):
+        return self(name)
+            
+    def array(self, dct=None, **kwargs):
+        try:
+            dct = kwargs if dct is None else dct
+            return np.array([dct[n] for n in self._dct.keys()])
+        except KeyError as e:
+            raise ValueError('Value not provided for required key: ' + str(e))
